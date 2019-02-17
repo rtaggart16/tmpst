9feb469b69fa73f06972cc31ca0c81ecd69ca900
rtaggart16 <rosstaggart44@gmail.com>
2019-02-17 15:11:12 +0000

    Add project files.

---
+++ tmpst/Models/UserSecretModels.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tmpst.Models
+{
+    /*
+       Author Info:
+
+       Name(s) - Ross Taggart
+       Student Number - S1828840
+       Date Created - 17/02/2019
+       Version - 1.0.0
+
+       Description:
+       Class that contains all the models which map to the secrets.json file
+     */
+
+    /// <summary>
+    /// Class which contains the API key required to communicate with the APIXU API
+    /// </summary>
+    public class ApixuConfiguration
+    {
+        public string API_Key { get; set; }
+    }
+
+    /// <summary>
+    /// Class which contains all of the Earthquake API URLs
+    /// </summary>
+    public class AllEarthquakeModels
+    {
+        public EarthquakeHourlyUrls HourlyUrls { get; set; }
+        public EarthquakeDailyUrls DailyUrls { get; set; }
+        public EarthquakeWeeklyUrls WeeklyUrls { get; set; }
+        public EarthquakeMonthlyUrls MonthlyUrls { get; set; }
+    }
+
+    /// <summary>
+    /// Class which contains all of the hourly Earthquake API URLs
+    /// </summary>
+    public class EarthquakeHourlyUrls
+    {
+        public string Significant_Quakes { get; set; }
+        public string Magnitude_Four_Point_Five { get; set; }
+        public string Magnitude_Two_Point_Five { get; set; }
+        public string Magnitude_One { get; set; }
+        public string All_Earthquakes { get; set; }
+    }
+
+    /// <summary>
+    /// Class which contains all of the daily Earthquake API URLs
+    /// </summary>
+    public class EarthquakeDailyUrls
+    {
+        public string Significant_Quakes { get; set; }
+        public string Magnitude_Four_Point_Five { get; set; }
+        public string Magnitude_Two_Point_Five { get; set; }
+        public string Magnitude_One { get; set; }
+        public string All_Earthquakes { get; set; }
+    }
+
+    /// <summary>
+    /// Class which contains all of the weekly Earthquake API URLs
+    /// </summary>
+    public class EarthquakeWeeklyUrls
+    {
+        public string Significant_Quakes { get; set; }
+        public string Magnitude_Four_Point_Five { get; set; }
+        public string Magnitude_Two_Point_Five { get; set; }
+        public string Magnitude_One { get; set; }
+        public string All_Earthquakes { get; set; }
+    }
+
+    /// <summary>
+    /// Class which contains all of the monthly Earthquake API URLs
+    /// </summary>
+    public class EarthquakeMonthlyUrls
+    {
+        public string Significant_Quakes { get; set; }
+        public string Magnitude_Four_Point_Five { get; set; }
+        public string Magnitude_Two_Point_Five { get; set; }
+        public string Magnitude_One { get; set; }
+        public string All_Earthquakes { get; set; }
+    }
+
+    /// <summary>
+    /// Class which contains the base URL for the Population API
+    /// </summary>
+    public class PopulationAPIUrl
+    {
+        public string Base_Url { get; set; }
+    }
+
+    /// <summary>
+    /// Class which contains the App ID and the App Code of the Traffic API
+    /// </summary>
+    public class TrafficAPIKeys
+    {
+        public string App_ID { get; set; }
+        public string App_Code { get; set; }
+    }
+}
---
+++ tmpst/Startup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using tmpst.Data;
+using tmpst.Models;
+using tmpst.Services;
+
+namespace tmpst
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.AddMvc();
+
+            // Secrets
+            services.Configure<ApixuConfiguration>(Configuration.GetSection("ApixuKey"));
+            services.Configure<EarthquakeHourlyUrls>(Configuration.GetSection("EarthquakeHourlyUrls"));
+            services.Configure<EarthquakeDailyUrls>(Configuration.GetSection("EarthquakeDailyUrls"));
+            services.Configure<EarthquakeWeeklyUrls>(Configuration.GetSection("EarthquakeWeeklyUrls"));
+            services.Configure<EarthquakeMonthlyUrls>(Configuration.GetSection("EarthquakeMonthlyUrls"));
+            services.Configure<PopulationAPIUrl>(Configuration.GetSection("PopulationAPIUrl"));
+            services.Configure<TrafficAPIKeys>(Configuration.GetSection("TrafficAPIKeys"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
