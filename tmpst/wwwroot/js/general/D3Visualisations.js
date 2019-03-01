5d1b996f31716d73e65bc1cffd20972b4d21d8d7
Aidan Marshall <36016925+aidanm1999@users.noreply.github.com>
2019-03-01 10:44:52 +0000

    Created the frame for visualisations

---
+++ tmpst/wwwroot/js/general/D3Visualisations.js
@@ -0,0 +1,51 @@
+﻿function createD3Visualisation(data, D3Type) {
+
+    var para = document.createElement("p");
+    var node = document.createTextNode("Chart");
+    para.appendChild(node);
+
+    var chart = document.getElementById("population-chart");
+    chart.appendChild(para);
+
+    console.log(data, D3Type);
+
+    //Chooses Visualisation
+    switch (D3Type){
+        case D3Type = "show-reel":
+            showReelBuilder(data);
+            break;
+        case D3Type = "bubble-chart":
+            bubbleChartBuilder(data);
+            break;
+        case D3Type = "radar-chart":
+            radarChartBuilder(data);
+            break;
+        case D3Type = "multi-packaging":
+            multiPackagingBuilder(data);
+            break;
+        default:
+            Swal.fire({
+                type: 'error',
+                title: 'Data Request Error',
+                text: 'An error has occurred when submitting your request. Please try again with different criteria'
+            });
+            break;
+    }
+}
+
+
+function showReelBuilder(data) {
+    //Laura - this function is for your visualisation
+}
+
+function bubbleChartBuilder(data) {
+    //Laura - this function is for your visualisation
+}
+
+function radarChartBuilder(data) {
+    //Aidan - this function is for your visualisation
+}
+
+function multiPackagingBuilder(data) {
+    //Aidan - this function is for your visualisation
+}
