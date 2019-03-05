522cd3a7871a6c0000304ef74e82e1147faf1f51
rtaggart16 <rosstaggart44@gmail.com>
2019-03-05 13:35:25 +0000

    Partial Changes to collapse and expand on weather system

--- tmpst/Startup.cs
+++ tmpst/Startup.cs
@@ -66,7 +66,7 @@ namespace tmpst
             app.UseStaticFiles();
 
             app.UseAuthentication();
-
+            
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
