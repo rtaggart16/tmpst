3ce62d7ae94adda8d0e930a2ce0528112193c453
Aidan Marshall <36016925+aidanm1999@users.noreply.github.com>
2019-03-11 18:02:14 +0000

    Finished pollution api

--- tmpst/wwwroot/js/pollution/pollution.js
+++ tmpst/wwwroot/js/pollution/pollution.js
@@ -1,10 +1,79 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
-    Highcharts.chart('container', {
+﻿function createPollutionChart(data) {
+
+    console.log(data.results);
+
+
+    // Arrays for different emmisions
+        //name: 'pm25',
+        //name: 'pm10',
+        //name: 'so2',
+        //name: 'no2',
+        //name: 'o3',
+        //name: 'co',
+        //name: 'bc',
+
+
+    let seriesArray = [0,0,0,0,0,0,0];
+
+    $(data.results).each(function (key, val) {
+
+        switch (val.parameter) {
+            case "pm25":
+                seriesArray[0] += val.value;
+                break;
+            case "pm10":
+                seriesArray[1] += val.value;
+                break;
+            case "so2":
+                seriesArray[2] += val.value;
+                break;
+            case "no2":
+                seriesArray[3] += val.value;
+                break;
+            case "o3":
+                seriesArray[4] += val.value;
+                break;
+            case "co":
+                seriesArray[5] += val.value;
+                break;
+            case "bc":
+                seriesArray[6] += val.value;
+                break;
+            default:
+            // code block
+        }
+    });
+
+    //$(seriesArray).each(function (key, val) {
+
+
+        
+    //});
+
+    
+
+    console.log("SERIES ARRAY--", seriesArray);
+       
+
+    Highcharts.chart('pollution-chart', {
         chart: {
             type: 'bar'
         },
+
         title: {
-            text: 'Air contaminents at your location'
+            text: ''
+        },
+        plotOptions: {
+            bar: {
+                minPointLength: 5,
+                dataLabels: {
+                    enabled: true,
+                    formatter: function () {
+                        if (this.y > 0)
+                            return this.y;
+                    }
+                }
+            }
         },
         xAxis: {
             categories: ['Type of Contaminent']
@@ -14,35 +83,39 @@
                 text: 'µg/m3'
             }
         },
-        series: [{
+        series:[{
             name: 'pm25',
-            data: [1]
+            data: [seriesArray[0]]
         },
         {
             name: 'pm10',
-            data: [1]
+            data: [seriesArray[1]]
         },
         {
             name: 'so2',
-            data: [1]
+            data: [seriesArray[2]]
         },
         {
             name: 'no2',
-            data: [1]
+            data: [seriesArray[3]]
         },
         {
             name: 'o3',
-            data: [1]
+            data: [seriesArray[4]]
         },
         {
             name: 'co',
-            data: [1]
+            data: [seriesArray[5]]
         },
 
         {
             name: 'bc',
-            data: [5]
+            data: [seriesArray[6]]
 
         }],
     });
-});
+}
+
+
+
+    
