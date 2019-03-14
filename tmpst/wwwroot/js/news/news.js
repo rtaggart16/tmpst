aa3bb5a6f864aa0df4ae54ffd2c5f698efab90f7
Aidan Marshall <36016925+aidanm1999@users.noreply.github.com>
2019-03-14 16:30:56 +0000

    Added script

--- tmpst/wwwroot/js/news/news.js
+++ tmpst/wwwroot/js/news/news.js
@@ -1,3 +1,15 @@
-﻿function newsVisualization(data) {
+﻿/*
+    Author Info:
+
+    Name(s) - Mark Riley, Ross Taggart, Aidan Marshall, Katie King
+    Student Number - S1829984, S1828840, S1828601, S1827986
+    Date Created - 25 / 02 / 2019
+    Version - 1.0.1
+
+    Description:
+    JavaScript file which contains the functions and event handlers for the news system
+ */
+
+function newsVisualization(data) {
 
 } 
