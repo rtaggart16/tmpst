32d4d770a756ef002f6bd969f1473d65d3ef93e5
Aidan Marshall <36016925+aidanm1999@users.noreply.github.com>
2019-03-14 21:42:37 +0000

    Finished news vis

--- tmpst/wwwroot/js/news/news.js
+++ tmpst/wwwroot/js/news/news.js
@@ -10,6 +10,44 @@
     JavaScript file which contains the functions and event handlers for the news system
  */
 
-function newsVisualization(data) {
+function newsVisualization(data, country, category) {
 
+    var text = '';
+
+    $.each(data.articles, function (key, val) {
+        //Concatenates text string together from sources
+        text = text + "" + val.title;
+    });
+
+    var lines = text.split(/[,\.\- ]+/g),
+        data = Highcharts.reduce(lines, function (arr, word) {
+            var obj = Highcharts.find(arr, function (obj) {
+                return obj.name === word;
+            });
+            if (obj) {
+                obj.weight += 1;
+            } else {
+                obj = {
+                    name: word,
+                    weight: 1
+                };
+                arr.push(obj);
+            }
+            return arr;
+        }, []);
+
+    var first25Data = data.slice(0, 24);
+
+
+
+    Highcharts.chart('chart', {
+        series: [{
+            type: 'wordcloud',
+            data: first25Data,
+            name: 'Occurrences'
+        }],
+        title: {
+            text: category + ' Headlines in ' + country,
+        }
+    });
 } 
