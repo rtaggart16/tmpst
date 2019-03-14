6f790410a6e6019f99bf87965fe29ec4efab0c99
Katie King <kinggx42@googlemail.com>
2019-03-14 14:00:21 +0000

    /

--- tmpst/wwwroot/js/News/news.js
+++ tmpst/wwwroot/js/News/news.js
@@ -1,4 +1,16 @@
-﻿function newsVisualization (data){
+﻿/*
+    Author Info:
+
+    Name(s) - Mark Riley, Ross Taggart, Aidan Marshall, Katie King
+    Student Number - S1829984, S1828840, S1828601, S1827986
+    Date Created - 25 / 02 / 2019
+    Version - 1.0.1
+
+    Description:
+    JavaScript file which contains the functions and event handlers for the news system
+ */
+
+function newsVisualization(data) {
 
     var width = 960,
         height = 500,
@@ -107,3 +119,6 @@ function collide(alpha) {
         });
     };
 }
+
+
+
