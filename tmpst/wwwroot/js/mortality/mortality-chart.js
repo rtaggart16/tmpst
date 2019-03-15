c8e3b18eac1721101b09aeb622a938a7fdfc890d
Marc <30148234@cityofglacol.ac.uk>
2019-03-15 12:23:57 +0000

    Added Mortality percentage pit chart

---
+++ tmpst/wwwroot/js/mortality/mortality-chart.js
@@ -0,0 +1,76 @@
+﻿function createMortalityChart(country, data) {
+    let dataobject = [];
+    $.each(data, function (key, val) {
+        let mortalityobject = {
+            name: val.age, 
+            value: val.mortality_percent
+        };
+        dataobject.push(mortalityobject);
+    });
+    option = {
+        backgroundColor: '#2c343c',
+
+        title: {
+            text: 'Mortality Percentage(%) for ' + country,
+            left: 'center',
+            top: 20,
+            textStyle: {
+                color: '#ccc'
+            }
+        },
+
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+
+        visualMap: {
+            show: false,
+            min: 80,
+            max: 600,
+            inRange: {
+                colorLightness: [0, 1]
+            }
+        },
+        series: [
+            {
+                name: 'Mortality Percentage',
+                type: 'pie',
+                radius: '60%',
+                center: ['50%', '50%'],
+                data: dataobject.sort(function (a, b) { return a.value - b.value; }),
+                roseType: 'radius',
+                label: {
+                    normal: {
+                        textStyle: {
+                            color: 'rgba(255, 255, 255, 0.3)'
+                        }
+                    }
+                },
+                labelLine: {
+                    normal: {
+                        lineStyle: {
+                            color: 'rgba(255, 255, 255, 0.3)'
+                        },
+                        smooth: 0.2,
+                        length: 10,
+                        length2: 20
+                    }
+                },
+                itemStyle: {
+                    normal: {
+                        color: '#c23531',
+                        shadowBlur: 200,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                },
+
+                animationType: 'scale',
+                animationEasing: 'elasticOut',
+                animationDelay: function (idx) {
+                    return Math.random() * 200;
+                }
+            }
+        ]
+    };
+}
