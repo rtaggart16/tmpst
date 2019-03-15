140b26ca40fffc63b3dc2e46068472f3cdc74dbe
Marc <30148234@cityofglacol.ac.uk>
2019-03-15 11:17:21 +0000

    Added Mortality Javascript

--- tmpst/wwwroot/js/mortality/mortality.js
+++ tmpst/wwwroot/js/mortality/mortality.js
@@ -1,3 +1,25 @@
 ﻿function submitMortalityRequest(url) {
     console.log(url);
+
+    let country = $('#mortality-country-select').val();
+    let sex = $('#mortality-sex-select').val();
+
+    console.log(country + ' ' + sex);
+
+    let requestUrl = url + country + '/' + sex + '/' + 0 + '/today/';
+    console.log(requestUrl);
+
+    $.ajax({
+        type: "GET",
+        url: requestUrl,
+        dataType: "jsonp",
+        contentType: "application/json",
+        success: function (result) {
+            console.log(result);
+
+        },
+        error: function (errorResult) {
+
+        }
+    });
 }
