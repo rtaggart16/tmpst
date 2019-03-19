fec5135d4b2395ab0c2fde6a8561435f64e1aaf5
Katie King <kinggx42@googlemail.com>
2019-03-19 12:41:18 +0000

    added student ID and name to all files

--- tmpst/wwwroot/js/general/D3Visualisations.js
+++ tmpst/wwwroot/js/general/D3Visualisations.js
@@ -1,4 +1,18 @@
-﻿function createD3Visualisation(data, D3Type) {
+﻿/*
+    Author Info:
+
+    Name(s) - Ross Taggart, Katie King
+    Student Number - S1828840, S1827986
+    Date Created - 02/03/2019
+    Version - 1.0.0
+
+    Description:
+    JavaScript file which contains the functions and event handlers D3 visualization
+*/
+
+
+
+function createD3Visualisation(data, D3Type) {
 
 
     //Removes existing headers and sub-headers
